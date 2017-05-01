9ed359674a03c4a8cdf7cbb8afc9cd2c918a07a2
Kenan Philips <philipskenan@gmail.com>
2017-04-30 17:40:05 -0700

    added image upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :user
+
+  # default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "400x600#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
